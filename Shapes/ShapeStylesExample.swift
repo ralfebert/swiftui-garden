e3f4c21349faaf04ee1a642eb738b4f1fdd3d649
Ralf Ebert <info@ralfebert.de>
2025-09-16 13:53:12 +0200

    updated shape colors

--- Shapes/ShapeStylesExample.swift
+++ Shapes/ShapeStylesExample.swift
@@ -5,16 +5,19 @@ import SwiftUI
 
 struct ShapeStylesExample: View {
     @Environment(\.colorScheme) var colorScheme
-
     var body: some View {
         ZStack {
-            (colorScheme == .dark ? Color(hue: 0.78, saturation: 0.75, brightness: 0.2) : Color.yellow)
+            Image(.stripe)
+                .resizable(resizingMode: .tile)
                 .ignoresSafeArea()
 
             VStack(alignment: .leading, spacing: 16) {
 
+                Text(colorScheme == .dark ? "Dark Mode" : "Light Mode")
+                    .font(.headline)
+
                 VStack(alignment: .leading, spacing: 0) {
-                    colorRow(style: .primary, ".primary")
+                    colorRow(style: .primary, ".primary", textStyle: .background)
                     colorRow(style: .secondary, ".secondary")
                     colorRow(style: .tertiary, ".tertiary")
                     colorRow(style: .quaternary, ".quaternary")
@@ -23,8 +26,18 @@ struct ShapeStylesExample: View {
 
                 VStack(alignment: .leading, spacing: 0) {
                     colorRow(style: .background, ".background")
-                    colorRow(style: .background.secondary, ".background.secondary")
-                    colorRow(style: .background.tertiary, ".background.tertiary")
+                    colorRow(style: .background.secondary, ".background.secondary", notes: "(systemGray6)")
+                    colorRow(style: .background.tertiary, ".background.tertiary", notes: colorScheme == .dark ? "(systemGray5)" : "(white)")
+                    colorRow(style: .background.quaternary, ".background.quaternary", notes: colorScheme == .dark ? "(systemGray4)" : "(systemGray6)")
+                }
+
+                VStack(alignment: .leading, spacing: 0) {
+                    colorRow(style: Color(uiColor: .systemGray), "Color(uiColor: .systemGray)")
+                    colorRow(style: Color(uiColor: .systemGray2), "Color(uiColor: .systemGray2)")
+                    colorRow(style: Color(uiColor: .systemGray3), "Color(uiColor: .systemGray3)")
+                    colorRow(style: Color(uiColor: .systemGray4), "Color(uiColor: .systemGray4)")
+                    colorRow(style: Color(uiColor: .systemGray5), "Color(uiColor: .systemGray5)")
+                    colorRow(style: Color(uiColor: .systemGray6), "Color(uiColor: .systemGray6)")
                 }
 
                 VStack(alignment: .leading, spacing: 0) {
@@ -32,7 +45,6 @@ struct ShapeStylesExample: View {
                     colorRow(style: .red.secondary, ".red.secondary")
                     colorRow(style: .red.tertiary, ".red.tertiary")
                     colorRow(style: .red.quaternary, ".red.quaternary")
-                    colorRow(style: .red.quinary, ".red.quinary")
                 }
 
             }
@@ -42,12 +54,20 @@ struct ShapeStylesExample: View {
         }
     }
 
-    @ViewBuilder func colorRow(style: some ShapeStyle, _ title: String) -> some View {
-        HStack {
+    @ViewBuilder func colorRow(style: some ShapeStyle, _ title: String, notes: String? = nil, textStyle: some ShapeStyle = .primary) -> some View {
+        ZStack(alignment: .leading) {
             Rectangle()
                 .foregroundStyle(style)
-                .frame(width: 50, height: 50)
-            Text(title)
+            VStack(alignment: .leading) {
+                Text(title)
+                    .bold()
+                    .foregroundStyle(textStyle)
+                if let notes {
+                    Text(notes)
+                        .font(.footnote)
+                }
+            }
+            .padding(6)
         }
     }
 
